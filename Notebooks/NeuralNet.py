1f448ae74704a1958aa2f0be4d6e9e8b949bb2bc
costasmexis <costasmexis@gmail.com>
2024-04-01 14:35:19 +0300

    Update code

---
+++ Notebooks/NeuralNet.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+
+def preprocess_data(train_df: pd.DataFrame):
+    X = torch.tensor(train_df.drop(columns=['mu_max', 'Km', 'Y_XS']).values, dtype=torch.float32)
+    y = torch.tensor(train_df[['mu_max', 'Km', 'Y_XS']].values, dtype=torch.float32)
+    return X, y
+
+class NN:
+    def __init__(self):
+        self.model = nn.Sequential(
+            nn.Linear(16, 128),
+            nn.Tanh(),
+            nn.Linear(128, 128),
+            nn.Tanh(),
+            nn.Linear(128, 3)
+        )
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+        self.criterion = nn.MSELoss()
+
+    def forward(self, X):   
+        return self.model(X)
+    
+    def train_val_split(self, X, y, val_size: float = 0.2):
+        n_samples = X.shape[0]
+        n_val = int(n_samples * val_size)
+        indices = np.random.permutation(n_samples)
+        train_indices, val_indices = indices[n_val:], indices[:n_val]
+        X_train, y_train = X[train_indices], y[train_indices]
+        X_val, y_val = X[val_indices], y[val_indices]
+        return X_train, y_train, X_val, y_val
+    
+    def train(self, X_train, y_train, X_val=None, y_val=None):
+        self.model.train()
+        y_pred = self.model(X_train)
+        loss = self.criterion(y_pred, y_train)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        if X_val is not None and y_val is not None:
+            y_pred_val = self.model(X_val)
+            loss_val = self.criterion(y_pred_val, y_val)
+            return loss.item(), loss_val.item()
+        return loss.item()        
+    
+    def predict(self, X):
+        self.model.eval()
+        with torch.no_grad():
+            return self.model(X)
+        
--- Notebooks/dataset.py
+++ Notebooks/dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
+from tqdm import tqdm 
 
 from scipy.integrate import odeint, solve_ivp
 
@@ -20,11 +21,11 @@ class Dataset:
         
         self.y0 = [self.df['Biomass'].iloc[0], self.df['Glucose'].iloc[0]] # (X, S)
         
-        
     def get_data(self, exp: int = 1):
-        return self.df[self.df['exp'] == exp]
+        self.df = self.df[self.df['exp'] == exp]
+        self.y0 = [self.df['Biomass'].iloc[0], self.df['Glucose'].iloc[0]] # (X, S)
     
-    def __ode_func(t, y, mu_max, Km, Y_XS):
+    def __ode_func(self, t, y, mu_max, Km, Y_XS):
         X = y[0]
         S = y[1]
         mu = mu_max * S / (S + Km)
@@ -33,5 +34,35 @@ class Dataset:
     
     def solve_ode(self, mu_max, Km, Y_XS):
         t = np.linspace(self.t_start, self.t_end, self.n_samples)
-        sol = odeint(Dataset.__ode_func, self.y0, t, args=(mu_max, Km, Y_XS))
+        sol = solve_ivp(self.__ode_func, [self.t_start, self.t_end], self.y0, t_eval=t, args=(mu_max, Km, Y_XS))
         return sol
+    
+    def plot_solution(self, sol, plot_exp: bool = True):
+        fig, ax = plt.subplots(figsize=(6, 4))
+        if plot_exp:
+            ax.scatter(self.df['Time'], self.df['Glucose'], label='Glucose', color='blue')
+            ax.scatter(self.df['Time'], self.df['Biomass'], label='Biomass', color='green')
+        ax.plot(sol.t, sol.y[0], label='X', color='red')
+        ax.plot(sol.t, sol.y[1], label='S', color='orange')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Concentration')
+        ax.set_title('Concentration vs Time')
+        ax.legend()
+        ax.grid(True)
+        plt.show()
+        
+    def training_dataset(self, N: int = 1000):
+        columns = [f'X_{i}' for i in range(len(self.df))]  + [f'S_{i}' for i in range(len(self.df))] + ['mu_max', 'Km', 'Y_XS']
+        train = pd.DataFrame(columns=columns)
+        t_train = np.linspace(self.t_start, self.t_end, self.df.shape[0])
+        for i in tqdm(range(N)):
+            mu_max = np.random.uniform(0.1, 1)
+            Km = np.random.uniform(0.0001, 1)
+            Y_XS = np.random.uniform(0.1, 1) 
+            sol = solve_ivp(self.__ode_func, [self.t_start, self.t_end], self.y0, t_eval=t_train, args=(mu_max, Km, Y_XS))
+            y_train_X = sol.y[0]
+            y_train_S = sol.y[1]
+            train.loc[i] = np.concatenate([y_train_X, y_train_S, [mu_max, Km, Y_XS]])
+        # drop rows with negative values 
+        train = train[(train >= 0).all(1)]
+        return train
