2d42484ed600c4864f4c89d25eb1dc28c6ca7f99
costasmexis <costasmexis@gmail.com>
2024-04-09 16:37:57 +0300

    update

---
+++ Bioindustry/dataset.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from tqdm import tqdm 
+
+from scipy.integrate import odeint, solve_ivp
+
+PATH_TO_DATA = 'data_ceasar.csv'
+N_SAMPLES = 100 
+
+class Dataset:
+    def __init__(self):
+        self.df = pd.read_csv(PATH_TO_DATA)
+        self.t_start = self.df['Time'].iloc[0]
+        self.t_end = self.df['Time'].iloc[-1]
+        self.n_samples = N_SAMPLES
+        
+        self.y0 = [self.df['Biomass'].iloc[0], self.df['Glucose'].iloc[0]] # (X, S)
+        
+    def get_data(self, exp: int = 1):
+        self.df = self.df[self.df['BR'] == exp]
+        self.y0 = [self.df['Biomass'].iloc[0], self.df['Glucose'].iloc[0]] # (X, S)
+        self.t_start = self.df['Time'].iloc[0]
+        self.t_end = self.df['Time'].iloc[-1]
+    
+    def __ode_func(self, t, y, mu_max, Km, Y_XS):
+        X = y[0]
+        S = y[1]
+        mu = mu_max * S / (S + Km)
+        dydt = [mu * X, -1/Y_XS * mu * X]
+        return dydt        
+    
+    def solve_ode(self, mu_max, Km, Y_XS):
+        t = np.linspace(self.t_start, self.t_end, self.n_samples)
+        sol = solve_ivp(self.__ode_func, [self.t_start, self.t_end], self.y0, t_eval=t, args=(mu_max, Km, Y_XS))
+        return sol
+    
+    def plot_solution(self, sol, exp: int, plot_exp: bool = True):
+        fig, ax = plt.subplots(figsize=(6, 4))
+        if plot_exp:
+            ax.scatter(self.df['Time'], self.df['Glucose'], label='Glucose', color='blue')
+            ax.scatter(self.df['Time'], self.df['Biomass'], label='Biomass', color='green')
+        ax.plot(sol.t, sol.y[0], label='X', color='red')
+        ax.plot(sol.t, sol.y[1], label='S', color='orange')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Concentration')
+        ax.set_title('Concentration vs Time')
+        ax.legend()
+        ax.grid(True)
+        plt.savefig(f'solution_{exp}.png')
+        plt.close()
+        
+    def training_dataset(self, N: int = 1000):
+        columns = [f'X_{i}' for i in range(len(self.df))]  + [f'S_{i}' for i in range(len(self.df))] + ['mu_max', 'Km', 'Y_XS']
+        train = pd.DataFrame(columns=columns)
+        t_train = np.linspace(self.t_start, self.t_end, self.df.shape[0])
+        for i in tqdm(range(N)):
+            mu_max = np.random.uniform(0.1, 1)
+            Km = np.random.uniform(0.0001, 1)
+            Y_XS = np.random.uniform(0.1, 1) 
+            sol = solve_ivp(self.__ode_func, [self.t_start, self.t_end], self.y0, t_eval=t_train, args=(mu_max, Km, Y_XS))
+            y_train_X = sol.y[0]
+            y_train_S = sol.y[1]
+            train.loc[i] = np.concatenate([y_train_X, y_train_S, [mu_max, Km, Y_XS]])
+        # drop rows with negative values 
+        train = train[(train >= 0).all(1)]
+        return train
--- Bioindustry/main.py
+++ Bioindustry/main.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torchdiffeq import odeint, odeint_adjoint
+import matplotlib.pyplot as plt
 import os
+from scipy.integrate import odeint, solve_ivp
 
-files = os.listdir('./data/')
+from dataset import Dataset
+from PINN import PINN, get_loss
+
+files = os.listdir('../Bioindustry/data/')
 files = [f for f in files if f.startswith('BR')]
 files.sort()
 
@@ -19,102 +22,39 @@ def get_data(files: list) -> pd.DataFrame:
         dfs.append(df)
     return pd.concat(dfs)
 
-class Network(nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-        self.net = nn.Sequential(
-            nn.Linear(1, 10),
-            nn.Tanh(),
-            nn.Linear(10, 10),
-            nn.Tanh(),
-            nn.Linear(10, 2)
-        )
-        
-        self.mu_max = nn.Parameter(torch.tensor([0.5]))
-        self.K = nn.Parameter(torch.tensor([0.5]))
-        self.Y = nn.Parameter(torch.tensor([0.5]))
-        
-        for m in self.net.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, mean=0, std=0.1)
-                nn.init.constant_(m.bias, val=0)
-    
-    def forward(self, x):
-        return self.net(x)
-    
-def residuals(t, model: nn.Module):
-    t_min = t.min().item()
-    t_max = t.max().item()
-    t = torch.linspace(t_min, t_max, 100).view(-1,1)
-    t.requires_grad = True
-    y = model(t)
-    X = y[:, 0].view(-1,1)
-    S = y[:, 1].view(-1,1)
-    dX_dt = torch.autograd.grad(X, t, grad_outputs=torch.ones_like(X), create_graph=True)[0]
-    dS_dt = torch.autograd.grad(S, t, grad_outputs=torch.ones_like(S), create_graph=True)[0]
-    error_1 = dX_dt - model.mu_max * S * X / (model.K + S)
-    error_2 = dS_dt + model.Y * model.mu_max * X * S/ (model.K + S)
-    e_1 = torch.mean(error_1**2)
-    e_2 = torch.mean(error_2**2)
-    return e_1 + e_2 
-    
-
-def generate_traindata(df: pd.DataFrame):
-    t_train = df['Time'].values.astype(np.float32)
-    X_train = df['Biomass'].values
-    S_train = df['Glucose'].values  
-    y_train = np.hstack((X_train.reshape(-1, 1), S_train.reshape(-1, 1))).astype(np.float32)
-    return t_train, y_train
-    
-                
-############################################
 df = get_data(files)
 df = df[df['Batch'] == 0]
-print(df.shape)
+df.to_csv('./data_ceasar.csv')
 
-### Modeling ###
-network = Network()
-optimizer = torch.optim.Adam(network.parameters(), lr=1e-4)
+d = Dataset()
+d.get_data(exp=2)
+
+pinn = PINN(1, 2, T_START=d.df['Time'].min(), T_END=d.df['Time'].max())
+optimizer = torch.optim.Adam(pinn.parameters(), lr=1e-4)
 criterion = nn.MSELoss()
 
-EPOCHS = 10
+t_train = d.df['Time'].values
+u_train = d.df[['Biomass', 'Glucose']].values
+
+# Train data to tensor
+ts_train = torch.tensor(t_train, requires_grad=True).float().view(-1,1)
+us_train = torch.tensor(u_train, requires_grad=True).float()
+
+EPOCHS = 20000
 LOSS = []
 for epoch in range(EPOCHS):
-    loss_exp = 0
-    for exp in range(1, 10):
-        _ = df[df['BR'] == exp]
-        t_train, y_train = generate_traindata(_)
-        ts_train = torch.tensor(t_train, requires_grad=True, dtype=torch.float32).view(-1,1)
-        ys_train = torch.tensor(y_train, requires_grad=True, dtype=torch.float32)
+    u_pred = pinn(ts_train)
+    residual_pred = get_loss(pinn)
+    loss = criterion(u_pred, us_train)
+    loss += 0.5*residual_pred
+    LOSS.append(loss.item())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if epoch % 500 == 0:
+        print(f'Epoch: {epoch}, Loss: {loss.item()}, ODE Loss: {torch.mean(residual_pred).item()}')
+        print(f'mu_max: {pinn.mu_max.item()}, Km: {pinn.Km.item()}, Y_XS: {pinn.Y_XS.item()}')
+        
+sol_pinn = d.solve_ode(mu_max=pinn.mu_max.item(), Km=pinn.Km.item(), Y_XS=pinn.Y_XS.item())
+d.plot_solution(sol_pinn, 1)
 
-        # Training
-        optimizer.zero_grad()        
-        pred_y = network.forward(ts_train)
-        physics_loss = residuals(ts_train, network)
-        
-        loss = criterion(pred_y, ys_train)
-        loss += physics_loss
-        
-        loss_exp += loss.item()
-        loss.backward()
-        optimizer.step()
-        
-    avg_loss = loss_exp / 9
-    LOSS.append(avg_loss)
-        
-    if epoch % 100 == 0:
-        print(f'Epoch: {epoch} | Loss: {avg_loss}, \n.......mu_max: {network.mu_max.item()}, K: {network.K.item()}, Y: {network.Y.item()}')
-        
-# Plot LOSS
-plt.plot(LOSS)
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Learning Curve')
-plt.savefig('loss.png')
-plt.close()
-plt.plot(LOSS)
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Learning Curve')
-plt.savefig('loss.png')
-plt.close()
